41ea2d7203e42a6dfea6beb23e7accd01d971126
Egii <regidesoftian@gmail.com>
2022-06-22 19:36:55 +0700

    dump miui_MERLINGlobal_V12.5.1.0.RJOMIXM_84b071b256_11.0.zip

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Link="https://bigota.d.miui.com/V13.0.1.0.SJOCNXM/miui_MERLIN_V13.0.1.0.SJOCNXM_17602cbb55_12.0.zip"
+Link="https://bigota.d.miui.com/V12.5.1.0.RJOMIXM/miui_MERLINGlobal_V12.5.1.0.RJOMIXM_84b071b256_11.0.zip"
 FileName="$(pwd)/dump.zip"
 export DEBIAN_FRONTEND=noninteractive
 apt-get -y update && apt-get -y upgrade && apt-get install -y unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python3 python3-pip brotli liblz4-tool axel gawk aria2 detox wget
